95d80a3a51a634306213f739a61cdec58ad7f935
aungchooohp2015-design <aungchooo.hp2015@gmail.com>
2026-09-07 23:46:03 +0630

    Create     sw.js

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE = "golden-account-v1";
+
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./sw.js",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
